36201ec763d8c6485e19debaecae14e8132aef63
jiangbiao <1361205163@qq.com>
2024-07-11 10:37:09 +0800

    updata example/

---
+++ mono.py
@@ -0,0 +1,289 @@
+"""
+黑白图像 ascii 风格转换
+"""
+import numpy as np
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+from PIL import Image, ImageFont, ImageDraw, ImageFilter
+
+
+CHARS = '@W#$OEXC[(/?=^~_.` '
+
+CHARS = '我爱你个大猪头@W#$OEXC[(/?=^~_.` '
+
+
+def im2char(im, dsize):
+    im = cv2.resize(im, dsize=dsize, interpolation=cv2.INTER_AREA)
+    length = len(CHARS) - 1
+    im = np.int32(np.round(im / 255 * length))
+    output = []
+    for y in range(dsize[1]):
+        s = ""
+        for x in range(dsize[0]):
+            s += CHARS[im[y][x]]
+        # print(s)
+        output.append(s)
+    # print(output)
+    return '\n'.join(output)
+
+
+def im2char_re_2darray(im, dsize):
+    """
+    返回 char array，based on im2char()
+    """
+    im = cv2.resize(im, dsize=dsize, interpolation=cv2.INTER_AREA)
+    length = len(CHARS) - 1
+    im = np.int32(np.round(im / 255 * length))
+    output = []
+    for y in range(dsize[1]):
+        row = []
+        for x in range(dsize[0]):
+            row.append(CHARS[im[y][x]])
+        output.append(row)
+    return output
+
+
+def mono(input: str, output: str = None, num_lines: int = 100, equalize: bool = False, gaussblur: bool = False):
+    """output grayscale .txt file"""
+    path = Path(input)
+    im = cv2.imread(str(path))
+    im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+    
+    plt.figure()
+    plt.subplot(1,3,1)
+    plt.imshow(im, cmap='gray')
+    
+    # 直方图均衡化
+    if equalize:
+        im = cv2.equalizeHist(im)
+        plt.subplot(1,3,2)
+        plt.imshow(im, cmap='gray')
+        
+    # 加高斯模糊
+    if gaussblur:
+        im = cv2.GaussianBlur(im, ksize=(3,3), sigmaX=2, sigmaY=2)
+        plt.subplot(1,3,3)
+        plt.imshow(im, cmap='gray')
+        
+    plt.show()
+
+    height, width, *_ = im.shape
+    output_height = num_lines
+    output_width = round(width * 1.865 * output_height / height)
+    # output_height = round(height / 1.865 * output_width / width)
+    text = im2char(im, (output_width, output_height))
+    
+    if output is None:
+        output = path.with_name(path.stem + '_mono_output.txt')
+        
+    with open(output, 'w') as f:
+        f.write(text)
+        print('write success')
+        
+        
+def get_background(
+    choice: str, 
+    origin,         # 原始图片输入
+    width, 
+    height, 
+    glur: bool=False,
+):
+    """generate a canvas to print"""
+    if choice == 'transparent':
+        # 4-channel
+        return Image.fromarray(np.uint8(np.zeros((height, width, 4))))
+    elif choice == 'black':
+        # 黑色蒙版
+        return Image.fromarray(np.uint8(np.zeros((height, width, 3))))
+    elif choice == 'white':
+        img = Image.fromarray(np.uint8(np.ones((height, width, 3)) * 255))
+        return img
+    elif choice == 'red':
+        bg_nda = np.concatenate([
+            np.ones((height, width, 1)) * 255, np.zeros((height, width, 1)), np.zeros((height, width, 1))
+        ], axis=-1) 
+        img = Image.fromarray(np.uint8(bg_nda))
+        return img
+    elif choice == 'green':
+        bg_nda = np.concatenate([
+            np.zeros((height, width, 1)), np.ones((height, width, 1)) * 255, np.zeros((height, width, 1))
+        ], axis=-1) 
+        img = Image.fromarray(np.uint8(bg_nda))
+        return img
+    elif choice == 'blue':
+        bg_nda = np.concatenate([
+            np.zeros((height, width, 1)), np.zeros((height, width, 1)), np.ones((height, width, 1)) * 255
+        ], axis=-1) 
+        img = Image.fromarray(np.uint8(bg_nda))
+        return img
+    elif choice.startswith('customize'):
+        color_rgb = [int(i) for i in choice.split('_')[-3:]]
+        bg_nda = np.concatenate([
+            np.ones((height, width, 1)) * color_rgb[0], np.ones((height, width, 1)) * color_rgb[1], np.ones((height, width, 1)) * color_rgb[2]
+        ], axis=-1)
+        img = Image.fromarray(np.uint8(bg_nda))
+        return img
+    elif choice == 'mean':
+        mean = np.mean(np.array(origin)[:])
+        return Image.fromarray(np.uint8(np.ones((height, width, 3)) * mean))
+    elif choice.startswith('origin'):
+        # 原图加上透明度
+        opacity = float(choice[-1]) / 10
+        canvas = origin.resize((width, height), Image.BICUBIC).filter(
+            ImageFilter.GaussianBlur(25)
+        )
+        canvas = np.array(canvas)
+        canvas = np.uint8(canvas[:, :, 0:3] * opacity)
+        return Image.fromarray(canvas)
+    
+
+def mono_ret_image(
+    input: str, 
+    output: str = None, 
+    num_lines: int = 100, 
+    equalize: bool = False, 
+    gaussblur: bool = False,
+    background: str = 'white',
+    background_glur: bool = False,
+    char_color: tuple = (0, 0, 0),
+    out_height: int = None,
+    scale: float = None,
+    fontsize: int = 17,
+    fonttype: str = None,
+    hw_ratio: float = 1.865,
+    char_width: float = 8.8,
+):
+    """
+    改写原始 mono 函数，使其返回 image 而不是 text file
+    
+    Params:
+        hw_ratio: 字符高宽比
+        char_width: 画布中字符的宽（高则由 char_width * hw_ratio 得到）
+    """
+    input_path = Path(input)
+    origin = cv2.imread(str(input_path))
+    origin = cv2.cvtColor(origin, cv2.COLOR_RGB2GRAY)
+    height, width, *_ = origin.shape
+    print('image size:', height, width)
+    
+    # plt.figure()
+    # plt.subplot(2, 3, 1)
+    # plt.imshow(origin, cmap='gray')
+    
+    # 直方图均衡化
+    if equalize:
+        # 1 全局直方图均衡化 (Global Histogram Equalization)
+        origin = cv2.equalizeHist(origin)
+        
+        # 2 CLAHE 直方图均衡化
+        # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        # origin = clahe.apply(origin)
+        
+        # # 3 基于YCrCb色彩空间的直方图均衡化
+        # # 转换到 YCrCb 色彩空间
+        # ycrcb = cv2.cvtColor(cv2.imread(str(input_path)), cv2.COLOR_BGR2YCrCb)
+        # # 对Y通道进行直方图均衡化
+        # ycrcb[:, :, 0] = cv2.equalizeHist(ycrcb[:, :, 0])
+        # # 转换回 BGR 色彩空间
+        # origin = cv2.cvtColor(ycrcb, cv2.COLOR_YCrCb2BGR)
+        # origin = cv2.cvtColor(origin, cv2.COLOR_RGB2GRAY)
+        
+    # 加高斯模糊
+    if gaussblur:
+        origin = cv2.GaussianBlur(origin, ksize=(3, 3), sigmaX=2, sigmaY=2)
+        # plt.subplot(2, 3, 3)
+        # plt.imshow(origin, cmap='gray')
+        
+    # 输出尺寸计算
+    output_height_rows = num_lines
+    output_width_cols = round(width * hw_ratio * output_height_rows / height)    # 1.865 为所选字符的宽高比
+    # print(output_height_rows, output_width_cols)    # (100, 280)
+    
+    # 获取输出文本
+    text = im2char_re_2darray(origin, (output_width_cols, output_height_rows))
+    # print(len(text), len(text[0]))      # (100, 280)
+    
+    text_rows, text_cols = output_height_rows, output_width_cols
+    # 设置字体参数
+    if fonttype == 'zh':
+        # 中文字体
+        font = ImageFont.truetype('simhei.ttf', fontsize, encoding='utf-8')
+    else:
+        font = ImageFont.truetype('courbd.ttf', fontsize)
+    char_height = char_width * hw_ratio
+    canvas_height = round(text_rows * char_height)
+    canvas_width = round(text_cols * char_width)
+    
+    # 2 构建画布
+    # a canvas used to draw texts on it
+    canvas = get_background(background, origin, canvas_width, canvas_height, background_glur)
+    # plt.subplot(2, 3, 5)
+    # plt.imshow(canvas)
+    
+    draw = ImageDraw.Draw(canvas)
+    for i in range(text_cols):
+        for j in range(text_rows):
+            x = round(char_width * i)
+            y = round(char_height * j)
+            char = text[j][i]
+            color = char_color       # 字体颜色
+            draw.text((x, y), char, fill=color, font=font)
+    
+    # resize the reproduct if necessary
+    if out_height:  # height goes first
+        canvas_height = out_height
+        canvas_width = round(width * canvas_height / height)
+        canvas = canvas.resize((canvas_width, canvas_height), Image.BICUBIC)
+    elif scale:
+        canvas_width = round(width * scale)
+        canvas_height = round(height * scale)
+        canvas = canvas.resize((canvas_width, canvas_height), Image.BICUBIC)
+        
+    # output filename
+    if output:
+        output_path = Path(output)
+    else:
+        output_path = input_path.with_name(
+            f'{input_path.stem}_{canvas_width}x{canvas_height}_D{text_rows}_{background}.png'
+        )
+    canvas.save(output_path)
+    
+    # plt.subplot(2, 3, 5)
+    # plt.imshow(canvas)
+    
+    # plt.show()
+    
+    print(f'Transformation completed. Saved as {output_path.name}.')
+
+    
+
+if __name__ == '__main__':
+    # test
+    # input_img = 'test_imgs/p1.jpg'
+    # output_txt = input_img.split('.')[0] + '_mono_output.txt'
+    # mono(input_img, output_txt, equalize=True, gaussblur=True)
+    
+    # test for mono_ret_image
+    # image = 'example/p1.jpg'
+    # image = 'test_imgs/p182_c.jpg'
+    # image = 'test_imgs/p165_c.jpg'
+    image = 'test_imgs/p72_c.jpg'
+    # image = 'test_imgs/p0_c.jpg'
+    w, h = Image.open(image).convert('RGB').size
+    output_image = image.split('.')[0] + '_mono_output.jpg'
+    kwargs = {
+        'num_lines': 160,           # 字符行数，行数越大，细节越清晰
+        'equalize': True,           # 直方图均衡化
+        'gaussblur': True,          # 高斯模糊
+        'background': 'white',      # 背景版颜色
+        # 'background': 'customize_255_255_0',      # 背景版颜色，自定义三个通道的颜色
+        # 'background': 'customize_17_238_238',
+        'background_glur': False,   # 是否对背景板做模糊处理
+        'fontsize': 17,             # 字体大小
+        'fonttype': 'zh',             # 字体类型，区分中英文
+        'char_color': (0, 0, 0),    # 字体颜色
+        'out_height': None,         # 输出图片高度
+        'char_width': 8.8,          # 字符宽度（和 fontsize 一起控制在底板上绘制字符的大小，char_width 越大，单个字符在底板上占据的空间就越大，此时若 fontsize 固定，则 width_size 越大，显示的字就越小）
+    }
+    mono_ret_image(image, output_image, **kwargs)
---
+++ video_utils.py
@@ -0,0 +1,105 @@
+# 图像&文件预处理函数
+
+import os
+import cv2
+from PIL import Image
+import numpy as np
+from tqdm import tqdm
+
+
+def video_frames_extract(video_path, output_folder, ff=None):
+    """
+    拆帧
+    :param video_path:
+    :param output_folder:
+    :param fps:
+    :return:
+    """
+    # 获取视频文件名
+    video_name = os.path.splitext(os.path.basename(video_path))[0]
+    # 新建文件夹
+    output_path = output_folder
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    # 打开视频
+    cap = cv2.VideoCapture(video_path)
+    # 获取&设置帧率
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print('视频默认帧率：', fps)
+    if ff is None:
+        # 设置帧间隔为 1，即每帧的图片都会被抽取出来
+        frame_interval = 1
+    else:
+        frame_interval = int(ff)
+    print('设置帧间隔：', frame_interval)
+
+    # 逐帧提取并保存
+    count = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            if count % frame_interval == 0:
+                image_name = os.path.join(output_path, f"{video_name}_frame_{count}.jpg")
+                cv2.imwrite(image_name, frame)
+            count += 1
+        else:
+            break
+    cap.release()
+    return fps
+
+
+def frames_to_video(frames_path, video_path, fps=25):
+    """
+    组帧
+    :param frames_path:
+    :param video_path:
+    :param fps: 每秒多少帧，推荐和原视频保持一致
+    :return:
+    """
+    im_list = os.listdir(frames_path)
+    # print(im_list)
+    im_list.sort(key=lambda x: int(x.split('.')[0].split('_')[2]))  # 最好再看看图片顺序对不
+    img = Image.open(os.path.join(frames_path, im_list[0]))
+    img_size = img.size  # 获得图片分辨率，im_dir文件夹下的图片分辨率需要一致
+    # print('image size:', img_size)
+
+    # fourcc = cv2.cv.CV_FOURCC('M','J','P','G')      #opencv版本是2
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')        # opencv版本是3
+    videoWriter = cv2.VideoWriter(video_path, fourcc, fps, img_size)
+    for i in tqdm(im_list):
+        im_name = os.path.join(frames_path, i)
+        # frame = cv2.imdecode(np.fromfile(im_name, dtype=np.uint8), -1)
+        frame = cv2.imread(im_name)
+        videoWriter.write(frame)
+    videoWriter.release()
+    print('finish')
+
+
+if __name__ == '__main__':
+    # 拆帧
+    # video_path = r'datasets\photo2ascii_self_size150_v2\self_test_videos\pkq.mp4'  # 视频文件路径
+    # output_folder = r'datasets\photo2ascii_self_size150_v2\self_test_videos\pkq_frames'  # 输出文件夹路径
+    # video_path = r'datasets\photo2ascii_self_size150_v2\self_test_videos\bs.mp4'
+    # output_folder = r'datasets\photo2ascii_self_size150_v2\self_test_videos\bs_frames'
+    # video_path = r'datasets\photo2ascii_self_size150_v2\self_test_videos\sl.mp4'
+    # output_folder = r'datasets\photo2ascii_self_size150_v2\self_test_videos\sl_frames'
+    # video_path = r'./bs.mp4'
+    # output_folder = r'./bs_frames'
+    # video_frames_extract(video_path, output_folder, ff=None)
+
+    # 组帧
+    # v1
+    # frames_path = r'D:\D1\AI_toys\CV\vision_transfor\pytorch-CycleGAN-and-pix2pix-master\pytorch-CycleGAN-and-pix2pix-master\datasets\photo2ascii_self\self_test_videos\pkq2_frames_ascii'
+    # video_path = r'D:\D1\AI_toys\CV\vision_transfor\pytorch-CycleGAN-and-pix2pix-master\pytorch-CycleGAN-and-pix2pix-master\datasets\photo2ascii_self\self_test_videos\pkq2_ascii.mp4v'
+    # frames_path = r'datasets\photo2ascii_self\self_test_videos\bs2_frames_ascii'
+    # video_path = r'datasets\photo2ascii_self\self_test_videos\bs2_ascii.mp4v'
+    # v2
+    # frames_path = r'datasets\photo2ascii_self_size150_v2\self_test_videos\bs_frames_ascii'
+    # video_path = r'datasets\photo2ascii_self_size150_v2\self_test_videos\bs_ascii.mp4v'
+    # frames_path = r'datasets\photo2ascii_self_size150_v2\self_test_videos\pkq_frames_ascii'
+    # video_path = r'datasets\photo2ascii_self_size150_v2\self_test_videos\pkq_ascii.mp4v'
+    # frames_path = r'datasets\photo2ascii_self_size150_v2\self_test_videos\sl_frames_ascii'
+    # video_path = r'datasets\photo2ascii_self_size150_v2\self_test_videos\sl_ascii.mp4v'
+    frames_path = r'bs_frames'
+    video_path = r'bs_ascii.mp4'
+    frames_to_video(frames_path, video_path, fps=59)
